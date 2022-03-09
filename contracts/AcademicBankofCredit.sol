e54835d7ed8714cdc9453dbf4108908b0dd3755e
Shiv Bhonde <shivbhonde04@gmail.com>
2022-03-10 02:11:49 +0530

    half done

--- contracts/AcademicBankofCredit.sol
+++ contracts/AcademicBankofCredit.sol
@@ -5,45 +5,98 @@ import "hardhat/console.sol";
 
 contract AcademicBankOfCredit {
 
-    struct University{
-        string name;
-        Department department;
-    }
+    enum Applied{notApplied,pending,Approved,rejected}
 
-    struct Department{
-        string departmentName;
-        Year year;
+    struct Department {
+        string name;
+        mapping(uint => Course) courses;
+        uint courseCount;
     }
 
     struct Course{
         string courseName;
         uint maxCredit;
+        uint year;
+    } 
+
+    struct University{
+        string name;
+        address univAddress;
+        mapping(uint => Department) departments;
+        uint departCount;
     }
-
-    struct Year{
-        Course course;
-        string[] reqCourses;
-        uint reqCredits;
-        uint minSeats;
-        Faculty faculty;
-    }
-
-    struct Faculty{
-        string facultyName;
-    }    
-
-    enum Applied{notApplied,Applied,pending,rejected}
 
     struct Student{
+        address univAddress;
+        address studentAddress;
         string studentName;
-        string studentAddress;
         uint currentYear;
-        string department;
+        string departmentName;
         uint totalCredits;
-        string[] courses;
+        mapping(uint => Course) completeCourses;
+        uint completeCount;
+        mapping(uint => Course) inCompleteCourses;
+        uint inCompleteCount;
+        mapping(uint => Course) pendingCourses;
+        uint pendingCourseCount;
         Applied status;
     }
 
+    mapping(address => University) public universites;
+    address[] listOfUniAddr;
+
+    mapping(address => Student) public students;
+    address[] listOfStuAddr;
+
+    function addUniversity(string memory _name)  public{
+        University storage u1 = universites[msg.sender];
+        u1.name = _name;
+        u1.univAddress = msg.sender;
+        Department storage d1 = u1.departments[u1.departCount];
+        u1.departCount++;
+        d1.name = "IT";
+        Course storage c1 = d1.courses[d1.courseCount];
+        d1.courseCount++;
+        c1.courseName="CHEM";
+        c1.maxCredit=5;
+        c1.year= 1;
+        listOfUniAddr.push(msg.sender);
+    }
+
+    function stringsEquals(string memory s1, string memory s2) private pure returns (bool) {
+        bytes memory b1 = bytes(s1);
+        bytes memory b2 = bytes(s2);
+        uint256 l1 = b1.length;
+        if (l1 != b2.length) return false;
+        for (uint256 i=0; i<l1; i++) {
+            if (b1[i] != b2[i]) return false;
+        }
+        return true;
+    }
+
+    function addStudent(string memory _name, uint _year,string memory _departName, address _univAddress) public{
+        Student storage s1 = students[msg.sender];
+        s1.univAddress = _univAddress;
+        s1.studentAddress = msg.sender;
+        s1.studentName = _name;
+        s1.currentYear = _year;
+        s1.departmentName = _departName;
+        University storage un = universites[_univAddress];
+        for(uint i = 0; i < un.departCount; i++){
+            if(stringsEquals(un.departments[i].name , _departName)){
+                for(uint j = 0; j < un.departments[i].courseCount; j++){
+                    Course storage c1 = s1.inCompleteCourses[s1.inCompleteCount];
+                    s1.inCompleteCount++;
+                    c1.courseName = un.departments[i].courses[j].courseName;
+                    c1.maxCredit = un.departments[i].courses[j].maxCredit;
+                    c1.year = un.departments[i].courses[j].year;
+                }
+            }
+        }
+
+        listOfStuAddr.push(msg.sender);
+
+    }
 
 
 }
---
+++ dummy.js
@@ -0,0 +1,156 @@
+unv.name = 'Ayush Shah college of Engineering';
+unv.department.departmentName = 'Information Technology';
+
+unv.department.year.year = 1;
+unv.department.year.reqCredits = 0;
+unv.department.year.minSeats = 10;
+unv.department.course.courseName = 'Physics';
+unv.department.course.credits = 4;
+unv.department.course[1].courseName = 'Chemistry';
+unv.department.course[1].credits = 4;
+unv.department.course[2].courseName = 'Maths I';
+unv.department.course[2].credits = 5;
+unv.department.course[3].courseName = 'Mechanics';
+unv.department.course[3].credits = 5;
+
+unv.department.year[1].year = 2;
+unv.department.year[1].reqCredits = 18;
+unv.department.year[1].minSeats = 30;
+unv.department.year[1].course.courseName = 'JPL';
+unv.department.year[1].course.credits = 5;
+unv.department.year[1].course[1].courseName = 'OS';
+unv.department.year[1].course[1].credits = 4;
+unv.department.year[1].course[2].courseName =
+	'Formal Language and Automata Theory';
+unv.department.year[1].course[2].credits = 4;
+unv.department.year[1].course[3].courseName = 'Maths';
+unv.department.year[1].course[3].credits = 5;
+
+unv.department.year[2].year = 3;
+unv.department.year[2].reqCredits = 18;
+unv.department.year[2].minSeats = 30;
+unv.department.year[2].course.courseName = 'Data Warehousing and Mining';
+unv.department.year[2].course.credits = 5;
+unv.department.year[2].course[1].courseName = 'Artificial Intelligence';
+unv.department.year[2].course[1].credits = 3;
+unv.department.year[2].course[2].courseName = 'Soft Computing';
+unv.department.year[2].course[2].credits = 4;
+unv.department.year[2].course[3].courseName =
+	'Cryptography and Network Security';
+unv.department.year[2].course[3].credits = 4;
+
+unv.department.year[3].year = 4;
+unv.department.year[3].minSeats = 30;
+unv.department.year[3].reqCredits = 18;
+unv.department.year[3].course.courseName = 'Blockchain Technology';
+unv.department.year[3].course.credits = 5;
+unv.department.year[3].course[1].courseName = 'Quantum Computing';
+unv.department.year[3].course[1].credits = 4;
+unv.department.year[3].course[2].courseName = 'Gamification';
+unv.department.year[3].course[2].credits = 4;
+unv.department.year[3].course[3].courseName = 'Predictive Analysis';
+unv.department.year[3].course[3].credits = 3;
+
+unv.department[1].departmentName = 'Computer Science';
+
+unv.department[1].year.year = 1;
+unv.department[1].year.reqCredits = 0;
+unv.department[1].year.minSeats = 10;
+unv.department[1].year.course.courseName = 'Physics';
+unv.department[1].year.course.credits = 4;
+unv.department[1].year.course[1].courseName = 'Chemistry';
+unv.department[1].year.course[1].credits = 4;
+unv.department[1].year.course[2].courseName = 'Maths I';
+unv.department[1].year.course[2].credits = 5;
+unv.department[1].year.course[3].courseName = 'Mechanics';
+unv.department[1].year.course[3].credits = 5;
+
+unv.department[1].year[1].year = 2;
+unv.department[1].year[1].reqCredits = 18;
+unv.department[1].year[1].minSeats = 30;
+unv.department[1].year[1].course.courseName = 'JPL';
+unv.department[1].year[1].course.credits = 5;
+unv.department[1].year[1].course[1].courseName = 'Database Management Systems';
+unv.department[1].year[1].course[1].credits = 5;
+unv.department[1].year[1].course[2].courseName = 'Data Structures';
+unv.department[1].year[1].course[2].credits = 4;
+unv.department[1].year[1].course[3].courseName = 'Maths';
+unv.department[1].year[1].course[3].credits = 5;
+
+unv.department[1].year[2].year = 3;
+unv.department[1].year[2].reqCredits = 18;
+unv.department[1].year[2].minSeats = 30;
+unv.department[1].year[2].course.courseName = 'Data Warehousing and Mining';
+unv.department[1].year[2].course.credits = 4;
+unv.department[1].year[2].course[1].courseName = 'Machine Learning';
+unv.department[1].year[2].course[1].credits = 3;
+unv.department[1].year[2].course[2].courseName = 'Big Data Infrastructure ';
+unv.department[1].year[2].course[2].credits = 4;
+unv.department[1].year[2].course[3].courseName = 'Network Engineering';
+unv.department[1].year[2].course[3].credits = 4;
+
+unv.department[1].year[3].year = 4;
+unv.department[1].year[3].minSeats = 30;
+unv.department[1].year[3].reqCredits = 18;
+unv.department[1].year[3].course.courseName =
+	'Digital Signal and Image Processing';
+unv.department[1].year[3].course.credits = 5;
+unv.department[1].year[3].course[1].courseName =
+	'Artificial Intelligence and Soft Computing';
+unv.department[1].year[3].course[1].credits = 4;
+unv.department[1].year[3].course[2].courseName = 'Distributed Computing';
+unv.department[1].year[3].course[2].credits = 5;
+unv.department[1].year[3].course[3].courseName = 'Human Machine Interaction';
+unv.department[1].year[3].course[3].credits = 3;
+
+unv.department[1].departmentName = 'Mechanics';
+
+unv.department[1].year.year = 1;
+unv.department[1].year.reqCredits = 0;
+unv.department[1].year.minSeats = 10;
+unv.department[1].year.course.courseName = 'Physics';
+unv.department[1].year.course.credits = 4;
+unv.department[1].year.course[1].courseName = 'Chemistry';
+unv.department[1].year.course[1].credits = 4;
+unv.department[1].year.course[2].courseName = 'Maths I';
+unv.department[1].year.course[2].credits = 5;
+unv.department[1].year.course[3].courseName = 'Mechanics';
+unv.department[1].year.course[3].credits = 5;
+
+unv.department[1].year[1].year = 2;
+unv.department[1].year[1].reqCredits = 18;
+unv.department[1].year[1].minSeats = 30;
+unv.department[1].year[1].course.courseName = 'Engineering Thermodynamics';
+unv.department[1].year[1].course.credits = 5;
+unv.department[1].year[1].course[1].courseName = 'Manufacturing Processess';
+unv.department[1].year[1].course[1].credits = 5;
+unv.department[1].year[1].course[2].courseName = 'Kinematics of Machinery';
+unv.department[1].year[1].course[2].credits = 4;
+unv.department[1].year[1].course[3].courseName = 'Fluid Mechanics';
+unv.department[1].year[1].course[3].credits = 5;
+
+unv.department[1].year[2].year = 3;
+unv.department[1].year[2].reqCredits = 18;
+unv.department[1].year[2].minSeats = 30;
+unv.department[1].year[2].course.courseName = 'Automotive Prime Movers';
+unv.department[1].year[2].course.credits = 4;
+unv.department[1].year[2].course[1].courseName = 'Mechanical Vibrations ';
+unv.department[1].year[2].course[1].credits = 3;
+unv.department[1].year[2].course[2].courseName = 'Mechatronics';
+unv.department[1].year[2].course[2].credits = 4;
+unv.department[1].year[2].course[3].courseName = 'Power Engineering';
+unv.department[1].year[2].course[3].credits = 4;
+
+unv.department[1].year[3].year = 4;
+unv.department[1].year[3].minSeats = 30;
+unv.department[1].year[3].reqCredits = 18;
+unv.department[1].year[3].course.courseName =
+	'Production Planning and Control ';
+unv.department[1].year[3].course.credits = 5;
+unv.department[1].year[3].course[1].courseName = 'Product Lifecycle Management';
+unv.department[1].year[3].course[1].credits = 4;
+unv.department[1].year[3].course[2].courseName = 'Power Plant Engineering';
+unv.department[1].year[3].course[2].credits = 5;
+unv.department[1].year[3].course[3].courseName =
+	'Industrial Engineering and Management';
+unv.department[1].year[3].course[3].credits = 3;
